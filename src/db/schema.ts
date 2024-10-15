9def3e7ab0f491863bcc2731ff2578960e50fee3
Aydın Terzi <aydinterzi7@gmail.com>
2024-10-15 21:25:46 +0300

    schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,27 +1,40 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  uuid,
+  integer,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users_table", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  age: integer("age").notNull(),
-  email: text("email").notNull().unique(),
+export const votingSessions = pgTable("voting_sessions", {
+  id: uuid().defaultRandom().primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  creatorId: uuid("creator_id").notNull(), // Clerk tarafından sağlanan kullanıcı ID
+  duration: integer("duration").notNull(), // Süre dakika cinsinden
+  candidateCount: integer("candidate_count").notNull(), // Aday sayısı
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  isActive: boolean("is_active").default(true).notNull(), // Oylamanın aktif olup olmadığını belirtir
 });
 
-export const postsTable = pgTable("posts_table", {
-  id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
-  userId: integer("user_id")
-    .notNull()
-    .references(() => usersTable.id, { onDelete: "cascade" }),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .$onUpdate(() => new Date()),
+export const candidates = pgTable("candidates", {
+  id: uuid().defaultRandom().primaryKey(),
+  sessionId: uuid("session_id")
+    .references(() => votingSessions.id, { onDelete: "cascade" })
+    .notNull(), // Oylama oturumuyla ilişkilendirilen aday
+  name: varchar("name", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export const votes = pgTable("votes", {
+  id: uuid().defaultRandom().primaryKey(),
+  sessionId: uuid("session_id")
+    .references(() => votingSessions.id, { onDelete: "cascade" })
+    .notNull(), // Oylama oturumu
+  candidateId: uuid("candidate_id")
+    .references(() => candidates.id, { onDelete: "cascade" })
+    .notNull(), // Oy verilen aday
+  userId: uuid("user_id").notNull(), // Clerk tarafından sağlanan kullanıcı ID
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
